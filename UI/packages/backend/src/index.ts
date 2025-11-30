22fd851b371117959b3a95fce53f555b01c598a2
rou-cru <roura.cruz.al@gmail.com>
2025-11-30 14:22:26 +0000

    fix

--- UI/packages/backend/src/index.ts
+++ UI/packages/backend/src/index.ts
@@ -38,6 +38,9 @@ backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 backend.add(import('@backstage/plugin-catalog-backend-module-github'));
+backend.add(
+  import('@backstage/plugin-catalog-backend-module-github-org'),
+);
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
